c77bbddc01e8f03ec34bab54b5e2bc509675a09d
Dawla Izza <dawlaizza405@gmail.com>
2024-02-15 14:32:25 +0700

    Download dataset dan coba load di dashboard

--- pages/5_5️⃣_10122034 - Dawla Izza Al-Din Noor.py
+++ pages/5_5️⃣_10122034 - Dawla Izza Al-Din Noor.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from utils import init
+import pandas as pd
 
 st.set_page_config(page_title="Izza", page_icon="5️⃣", layout="wide")
 
 init()
 
 st.title("Dawla Izza Al-Din Noor")
+
+order_review_late = pd.read_csv('dataset/izza-order_review_late.csv')
+st.dataframe(order_review_late)
